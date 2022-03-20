e1b72156147b411adf71522310c2a5973c5f6351
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-03-20 14:56:14 +0600

    Api Controller created

---
+++ StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentAdminPortal.API.Repositories;
+
+namespace StudentAdminPortal.API.Controllers
+{
+    
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        private readonly IStudentRepository studentRepository;
+        public StudentsController(IStudentRepository studentRepository)
+        {
+            this.studentRepository = studentRepository;
+        }
+
+        [HttpGet]
+        [Route("Controller")]
+        public IActionResult GetAllStudent()
+        {
+            return Ok(studentRepository.GetStudents());
+        }
+    }
+}
