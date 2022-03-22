2f2b749a06dab6d30ed629c06e65124b01f4a62a
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-03-22 12:33:48 +0600

    Student details UI done

--- StudentAdminPortal.UI/studentadminportal-ui/src/app/app-routing.module.ts
+++ StudentAdminPortal.UI/studentadminportal-ui/src/app/app-routing.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentsComponent } from './students/students.component';
+import { ViewStudentComponent } from './students/view-student/view-student.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: StudentsComponent
+    component: StudentsComponent,
   },
   {
     path: 'students',
-    component: StudentsComponent
-  }
+    component: StudentsComponent,
+  },
+  {
+    path: 'students/:id',
+    component: ViewStudentComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- StudentAdminPortal.UI/studentadminportal-ui/src/app/students/students.component.ts
+++ StudentAdminPortal.UI/studentadminportal-ui/src/app/students/students.component.ts
@@ -8,38 +8,45 @@ import { StudentService } from './student.service';
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrls: ['./students.component.css']
+  styleUrls: ['./students.component.css'],
 })
 export class StudentsComponent implements OnInit {
   students: Student[] = [];
-  displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile', 'gender'];
+  displayedColumns: string[] = [
+    'firstName',
+    'lastName',
+    'dateOfBirth',
+    'email',
+    'mobile',
+    'gender',
+    'edit',
+  ];
   dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
   @ViewChild(MatPaginator) matPaginator!: MatPaginator;
   @ViewChild(MatSort) matSort!: MatSort;
   filterString = '';
 
-  constructor(private studentService: StudentService) { }
+  constructor(private studentService: StudentService) {}
 
   ngOnInit(): void {
     // Fetch Students
-    this.studentService.getStudent()
-      .subscribe(
-        (successResponse) => {
-          this.students = successResponse;
-          this.dataSource = new MatTableDataSource<Student>(this.students);
+    this.studentService.getStudents().subscribe(
+      (successResponse) => {
+        this.students = successResponse;
+        this.dataSource = new MatTableDataSource<Student>(this.students);
 
-          if (this.matPaginator) {
-            this.dataSource.paginator = this.matPaginator;
-          }
-
-          if (this.matSort) {
-            this.dataSource.sort = this.matSort;
-          }
-        },
-        (errorResponse) => {
-          console.log(errorResponse);
+        if (this.matPaginator) {
+          this.dataSource.paginator = this.matPaginator;
         }
-      );
+
+        if (this.matSort) {
+          this.dataSource.sort = this.matSort;
+        }
+      },
+      (errorResponse) => {
+        console.log(errorResponse);
+      }
+    );
   }
 
   filterStudents() {
